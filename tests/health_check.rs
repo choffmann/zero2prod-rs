ddc243fff2f50f2e08757039cda2e35d6c951d62
Cedrik Hoffmann <dev@choffmann.io>
2025-03-17 22:33:57 +0100

    refactor: spin up test instace with random port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,19 +1,28 @@
+//! tests/health_check.rs
+use std::net::TcpListener;
+
+/// Spin up an instance of our application
+/// and returns its address (i.e. http://127.0.0.1:XXXX)
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to execute request.");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
-    let _ = tokio::spawn(server);
 }
