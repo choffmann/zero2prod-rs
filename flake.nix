45fb4559860dbec31b8f12063225f5491dea4b32
Cedrik Hoffmann <dev@choffmann.io>
2025-06-05 22:41:58 +0200

    chore: update nix flakes

--- flake.nix
+++ flake.nix
@@ -1,96 +1,53 @@
 {
-  description = "Rust development environment";
+  description = "A Rust development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    rust-overlay,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        # Read the file relative to the flake's root
-        overrides = builtins.fromTOML (builtins.readFile (self + "/rust-toolchain.toml"));
-        overlays = [(import rust-overlay)];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        rustToolchain = pkgs.pkgsBuildBuild.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        buildInputs = with pkgs; [openssl];
-        nativeBuildInputs = with pkgs; [
-          # rustup
+  outputs = inputs: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+              inputs.self.overlays.default
+            ];
+          };
+        });
+  in {
+    overlays.default = final: prev: {
+      rustToolchain = final.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    };
+
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
           rustToolchain
+          openssl
           pkg-config
-          lld
-          sqlx-cli
+          cargo-deny
+          cargo-edit
           cargo-watch
+          rust-analyzer
+          sqlx-cli
           cargo-expand
           cargo-udeps
+          bunyan-rs
         ];
-      in {
-        devShells.default = pkgs.mkShell rec {
-          inherit buildInputs nativeBuildInputs;
 
-          # RUSTC_VERSION = overrides.toolchain.channel;
-
-          # shellHook = ''
-          #   export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-          #   export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-          # '';
-
-          # nativeBuildInputs = [pkgs.pkg-config];
-          # buildInputs = with pkgs; [
-          #   rust-analyzer
-          #   rustToolchain
-          #   clang
-          #   lld
-          #   llvmPackages.bintools
-          #   openssl
-          #
-          #   sqlx-cli
-          #   cargo-watch
-          #   cargo-expand
-          # ];
-
-          # RUSTC_VERSION = overrides.toolchain.channel;
-          #
-          # # https://github.com/rust-lang/rust-bindgen#environment-variables
-          # LIBCLANG_PATH = pkgs.lib.makeLibraryPath [pkgs.llvmPackages_latest.libclang.lib];
-          #
-          # shellHook = ''
-          #   export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-          #   export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-          # '';
-          #
-          # # Add precompiled library to rustc search path
-          # RUSTFLAGS = builtins.map (a: ''-L ${a}/lib'') [
-          #   # add libraries here (e.g. pkgs.libvmi)
-          # ];
-          #
-          # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
-          #
-          # # Add glibc, clang, glib, and other headers to bindgen search path
-          # BINDGEN_EXTRA_CLANG_ARGS =
-          #   # Includes normal include path
-          #   (builtins.map (a: ''-I"${a}/include"'') [
-          #     # add dev libraries here (e.g. pkgs.libvmi.dev)
-          #     pkgs.glibc.dev
-          #   ])
-          #   # Includes with special directory paths
-          #   ++ [
-          #     ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-          #     ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-          #     ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-          #   ];
+        env = {
+          # Required by rust-analyzer
+          RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
         };
-      }
-    );
+      };
+    });
+  };
 }
